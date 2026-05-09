2c5f29103df68a8692572ac2b1daa052e01cca28
Miguel <mangel.010@hotmail.com>
2026-05-09 14:51:37 -0500

    .

--- finca-service/src/main/java/com/agromag/finca/controllers/NovedadController.java
+++ finca-service/src/main/java/com/agromag/finca/controllers/NovedadController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import com.agromag.finca.services.NovedadService;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping("/novedades")
